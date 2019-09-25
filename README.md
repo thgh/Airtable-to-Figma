4c2de95909f1fdbb21e61db911d9e98311f5f438
Thomas Ghysels <info@thomasg.be>
2019-09-25 13:23:36 +0200

    Refactor to Vue
    Redesign

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
-Airtable Figma plugin
+Airtable to Figma
 ---
 
-1. Click the share icon in the Airtable view that contains your data.
-2. Create a shareable grid view link.
-3. Copy the link and paste it in this plugin.
-4. Select the columns you want to use.
-5. PROFIT!
+Load your data from Airtable in your components.
+
+1. Select a component that contains text nodes.
+2. Open Airtable to Figma.
+3. Insert a shareable view link of the data you want to use.
+4. Assign Airtable columns to text nodes.
+5. Done! For every row in your table, a component instance will be created.
+
+Features not yet implemented:
+* Option to limit and filter the data
+* Option to place the components in a grid
+* Save the view link so it's easy to update the data
+
+
+Changelog
+
+v4 - 2019-09-25
+* Add option to layout items horizontally, vertically or in a grid
+* Add option to limit the number of imported items
+* Add option to choose the gutter
+* Add support for image columns
+* Add support for number columns
+* Add example Airtable link
+* Immediately show results
+* Initially check every second for selection
+* Save link, mapping and config in figma node
+
+v3 - 2019-09-16
+* Update artwrok and description
--- code.js
+++ code.js
@@ -1,87 +1,186 @@
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
 const SPACING = 20;
-//console.log(this, figma)
+const requests = {};
+const imageCache = {};
+// console.log('selection', figma.currentPage.selection)
+// console.log(this, figma)
 figma.showUI(__html__, {
     width: 400,
     height: 233,
 });
 figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
-    console.log(msg);
-    if (msg.type === 'debug') {
-        console.log(figma.currentPage.selection);
+    const { data } = tryParse(msg);
+    if (msg.type === 'notify') {
+        return figma.notify(msg.data);
+    }
+    if (requests[msg.id]) {
+        requests[msg.id].resolve(data);
         return;
     }
-    if (msg === 'get-selection-texts' || msg.type === 'get-selection-texts') {
-        figma.ui.postMessage({
-            type: 'selection',
-            selection: figma.currentPage.selection,
-        });
-        figma.ui.postMessage({
-            type: 'selection-texts',
-            texts: getTexts(figma.currentPage.selection),
-        });
-        detectConfig();
+    if (msg.type === 'request:selection') {
+        for (var i = 0; i < 10; ++i) {
+            const selection = getOriginalSelection();
+            if (selection) {
+                return responseMessage(msg, [fromSelection(selection)]);
+            }
+            yield delay(1000);
+        }
+        const selection = getOriginalSelection();
+        if (selection) {
+            return responseMessage(msg, [fromSelection(selection)]);
+        }
+        return responseMessage(msg, []);
+    }
+    if (msg.type === 'request:texts') {
+        // responseMessage(msg, getTexts([getOriginalSelection()]))
+        // return
+        return figma.notify('deprecated');
+    }
+    if (msg.type === 'request:saveAirtableLink') {
+        figma.notify('link saved');
+        setPluginJSON('airtableLink', data || '');
+        responseMessage(msg, 'ok');
         return;
     }
-    if (msg.type === 'airtable-load') {
-        const { data, mapping } = msg;
-        const lookup = getArrayMapping(figma.currentPage.selection[0], mapping);
-        const instance = getFrame(figma.currentPage.selection[0]);
+    if (msg.type === 'request:saveAirtableData') {
+        setPluginJSON('airtableData', data || []);
+        responseMessage(msg, 'ok');
+        return;
+    }
+    if (msg.type === 'request:renderAirtable') {
+        const { airtableRows, airtableConfig } = data;
+        const { mapping, images } = airtableConfig;
+        const limit = airtableConfig.limit || 100;
+        const spacing = parseInt(airtableConfig.spacing) || 20;
+        const grid = ['columns', 'grid'].includes(airtableConfig.grid)
+            ? airtableConfig.grid
+            : 'rows';
+        const columnCount = airtableConfig.grid === 'grid'
+            ? parseInt(airtableConfig.columns) || 3
+            : airtableConfig.grid === 'columns'
+                ? 100
+                : 1;
+        const rowCount = Math.ceil(limit / columnCount) || 1;
+        const original = getOriginalSelection();
+        if (!original) {
+            responseMessage(msg, 'sync:selection');
+            console.log('selection', figma.currentPage.selection);
+            return figma.notify('No frame selected');
+        }
+        setPluginJSON('airtableConfig', airtableConfig);
+        const instance = getFrame(original);
         if (!instance) {
-            return console.warn('no frame');
+            responseMessage(msg, 'sync:selection');
+            console.log('selection', figma.currentPage.selection);
+            return figma.notify('No frame selected');
         }
-        console.warn('data', data);
-        if (!data) {
-            return console.warn('no data');
+        if (!airtableRows) {
+            console.warn('no rows', airtableRows);
+            responseMessage(msg, 'error');
+            return figma.notify('No rows from Airtable');
         }
-        yield figma.loadFontAsync({ family: 'Roboto', style: 'Regular' });
-        console.log('lo', lookup);
+        if (!mapping.find(col => col.airtableField)) {
+            console.warn('no fields mapped', mapping);
+            responseMessage(msg, 'error');
+            return figma.notify('No fields mapped');
+        }
+        const textLookup = getArrayMapping(original, mapping);
+        const imageLookup = getArrayMapping(original, images);
+        yield loadFonts(mapping);
+        // console.log('lo', textLookup)
+        const instanceId = instance.id;
+        instance.setPluginData('clonedFrom', instanceId);
         const parent = instance.parent;
         const focus = [];
         focus.push(instance);
-        let offset = instance.y;
+        let y = instance.y;
+        let x = instance.x;
+        let height = instance.height;
+        let width = instance.width;
         let first = true;
-        for (const rowData of data) {
-            const clone = first ? instance : instance.clone();
-            first = false;
-            const subs = clone.findAll(() => true);
-            console.log('subs', subs);
-            subs.forEach((sub, i) => interpolate(sub, rowData[lookup[i]]));
-            parent.appendChild(clone);
-            clone.y = offset;
-            offset += instance.height + SPACING;
-            focus.push(clone);
+        let i = 0;
+        for (let r = 0; r < rowCount; r++) {
+            for (let c = 0; c < columnCount && i < airtableRows.length && i < limit; c++) {
+                const rowData = airtableRows[i];
+                const clone = first
+                    ? instance
+                    : instance.type === 'COMPONENT'
+                        ? fromComponent(instance)
+                        : instance.clone();
+                first = false;
+                const subs = clone.findAll(() => true);
+                // console.log('subs', subs)
+                subs.forEach((sub, i) => interpolateText(sub, rowData[textLookup[i]]));
+                subs.forEach((sub, i) => interpolateImage(sub, rowData[imageLookup[i]]));
+                parent.appendChild(clone);
+                clone.y = y + (height + spacing) * r;
+                clone.x = x + (width + spacing) * c;
+                clone.setPluginData('clonedFrom', instanceId);
+                // clone.locked = true
+                focus.push(clone);
+                i++;
+            }
         }
         // TODO: saved
         // Update selection
         figma.currentPage.selection = focus;
-        figma.viewport.scrollAndZoomIntoView(focus);
+        // figma.viewport.scrollAndZoomIntoView(focus)
+        responseMessage(msg, 'ok');
+        return;
     }
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
     figma.closePlugin();
 });
+function loadFonts(mapping) {
+    return Promise.all(mapping.map(node => figma.getNodeById(node.id)).map(loadFont));
+}
+function loadFont(node) {
+    return node.type === 'TEXT' && figma.loadFontAsync(node.fontName);
+}
 function getArrayMapping(instance, mapping) {
     return instance
         .findAll(() => true)
         .map(node => mapping.find(n => n.id === node.id))
-        .map(f => f && f.field);
+        .map(f => f && f.airtableField);
 }
-function interpolate(node, value) {
+function interpolateText(node, value) {
     if (value) {
-        node.characters = value;
+        const { name } = node;
+        node.characters = String(value);
+        if (node.name === node.characters) {
+            node.name = name;
+        }
         return;
     }
     // if (node.type === 'TEXT') {
     //   node.characters = templateHash(node.characters, row)
     // }
+}
+function interpolateImage(node, value) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (value && value[0] && value[0].largeThumbUrl) {
+            const image = yield downloadImage(value[0].largeThumbUrl);
+            const paint = JSON.parse(JSON.stringify(node.fills[0]));
+            paint.imageHash = figma.createImage(new Uint8Array(image)).hash;
+            node.fills = [paint].concat(node.fills.slice(1));
+            return;
+        }
+    });
+}
+function downloadImage(url) {
+    if (!imageCache[url]) {
+        imageCache[url] = requestMessage('downloadImage', url);
+    }
+    return imageCache[url];
 }
 function templateHash(text, data) {
     if (data) {
@@ -91,25 +190,76 @@ function templateHash(text, data) {
     }
     return text;
 }
+function getOriginalSelection() {
+    const instance = figma.currentPage.selection[0];
+    if (!instance) {
+        return instance;
+    }
+    if (instance.type !== 'INSTANCE' &&
+        instance.type !== 'FRAME' &&
+        instance.type !== 'COMPONENT') {
+        return null;
+    }
+    const id = instance.getPluginData('clonedFrom');
+    return (id && figma.getNodeById(id)) || instance;
+}
 function getFrame(instance) {
     if (!instance) {
         return console.warn('no selection');
     }
+    // console.log('clonedfrm', instance, instance.getPluginData('clonedFrom'))
+    // Start from cloned node
+    // {
+    //   const id = instance.getPluginData('clonedFrom')
+    //   const node = figma.getNodeById(id)
+    //   if (node && id && id !== instance.id) {
+    //     console.log('indirect instance')
+    //     instance = node
+    //   } else if (id && !node) {
+    //     console.log('old clone')
+    //     instance.setPluginData('clonedFrom', null)
+    //   }
+    // }
     // Start from component
     if (instance.type === 'COMPONENT') {
-        const parent = instance;
-        instance = instance.createInstance();
-        // To the right
-        instance.x = parent.x + parent.width + SPACING;
-        instance.y = parent.y;
+        // const parent = instance
+        // instance = instance.createInstance()
+        // // To the right
+        // instance.x = parent.x + parent.width + SPACING
+        // instance.y = parent.y
+        // figma.currentPage.selection = [instance]
+        // figma.viewport.scrollAndZoomIntoView(focus)
     }
-    // Start from component
+    // Start from frame
     if (instance.type === 'FRAME') {
-        const parent = instance;
-        instance = instance.clone();
-        // To the right
-        instance.x = parent.x + parent.width + SPACING;
-        instance.y = parent.y;
+        // Use frame as is
+        // const parent = instance
+        // instance = instance.clone()
+        // // To the right
+        // instance.x = parent.x + parent.width + SPACING
+        // instance.y = parent.y
+    }
+    // Start from cloned node
+    const id = instance.getPluginData('clonedFrom');
+    const node = figma.getNodeById(id);
+    if (node && id && id !== instance.id) {
+        console.log('indirect instance');
+        instance = node;
+    }
+    else if (id && !node) {
+        console.log('old clone');
+        instance.setPluginData('clonedFrom', null);
+    }
+    if (node) {
+        const toRemove = [];
+        const id = instance.id;
+        traverse(figma.currentPage, node => {
+            if (node.getPluginData('clonedFrom') === id && node.id !== id) {
+                toRemove.push(node);
+            }
+        });
+        toRemove.forEach(node => node.remove());
+        // console.log('toremo', toRemove.length)
     }
     if (!instance ||
         (instance.type !== 'INSTANCE' &&
@@ -117,6 +267,13 @@ function getFrame(instance) {
             instance.type !== 'COMPONENT')) {
         return console.warn('no instance');
     }
+    return instance;
+}
+function fromComponent(parent) {
+    const instance = parent.createInstance();
+    // To the right
+    instance.x = parent.x + parent.width + SPACING;
+    instance.y = parent.y;
     return instance;
 }
 function getTexts(layer) {
@@ -134,6 +291,22 @@ function getTexts(layer) {
     });
     return texts;
 }
+function getImages(layer) {
+    if (!layer)
+        return [];
+    const images = [];
+    traverse(layer, layer => {
+        if (layer.fills && layer.fills[0] && layer.fills[0].type === 'IMAGE') {
+            images.push({
+                id: layer.id,
+                name: layer.name,
+                imageHash: layer.fills[0].imageHash,
+            });
+        }
+    });
+    console.log('getImages', images);
+    return images;
+}
 function traverse(node, func) {
     if (Array.isArray(node)) {
         return node.map(n => traverse(n, func));
@@ -149,4 +322,72 @@ function detectConfig() {
     // To implement
     // Read selection metadata
     // post metadata to plugin
+}
+function requestMessage(type, data) {
+    // Store all requests in one object and handle in global onmessage
+    const id = Math.random().toString(36);
+    return new Promise(resolve => {
+        console.log('requestmessage', type, data);
+        requests[id] = { resolve };
+        figma.ui.postMessage({
+            id,
+            type: 'request:' + type,
+            data: JSON.stringify(data),
+        });
+    });
+}
+// function requestMessage(msg, data) {
+//   figma.ui.postMessage({
+//     id: msg.id,
+//     type: msg.type.replace('request:', 'response:'),
+//     data: JSON.stringify(data),
+//   })
+// }
+function responseMessage(msg, data) {
+    figma.ui.postMessage({
+        id: msg.id,
+        type: msg.type.replace('request:', 'response:'),
+        data: JSON.stringify(data),
+    });
+}
+function fromSelection(node) {
+    return {
+        id: node.id,
+        type: node.type,
+        airtableLink: getPluginJSON(node, 'airtableLink'),
+        airtableData: getPluginJSON(node, 'airtableData'),
+        airtableConfig: getPluginJSON(node, 'airtableConfig'),
+        texts: getTexts(node),
+        images: getImages(node),
+    };
+}
+function getPluginJSON(node, key) {
+    const data = node.getPluginData(key);
+    try {
+        return JSON.parse(data);
+    }
+    catch (e) { }
+    return data;
+}
+function setPluginJSON(key, data) {
+    ;
+    []
+        .concat(figma.currentPage.selection, getOriginalSelection())
+        .filter(Boolean)
+        .map(node => {
+        node.setPluginData(key, JSON.stringify(data));
+    });
+}
+function tryParse(msg) {
+    try {
+        return {
+            type: msg.type,
+            data: JSON.parse(msg.data),
+        };
+    }
+    catch (e) { }
+    return msg || {};
+}
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
